170cea20e7da8357e9f722c1342141446fa2c23c
aquarius <chenmengyan93@163.com>
2022-08-15 21:52:49 +0800

    init

---
+++ machine-state/src/main/java/com/example/machinestate/MachineStateApplication.java
@@ -0,0 +1,13 @@
+package com.example.machinestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineStateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MachineStateApplication.class, args);
+    }
+
+}
